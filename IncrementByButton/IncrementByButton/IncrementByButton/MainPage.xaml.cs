186c0b268820ef5fba01dc2047fbeab19d8da76b
muhammedikbalaktas <muhammedikbalaktas@gmail.com>
2022-06-11 16:44:28 +0300

    proje

---
+++ IncrementByButton/IncrementByButton/IncrementByButton/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace IncrementByButton
+{
+    public partial class MainPage : ContentPage
+    {
+        static int num = 0;
+        
+        public MainPage()
+        {
+            
+
+
+            InitializeComponent();
+
+            
+            
+        }
+
+        void my_click(object sender, EventArgs args)
+        {
+            num++;
+            text_for_increment.Text = "" + num;
+
+        }
+
+
+    }
+    
+
+
+}
+
